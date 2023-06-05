1b24b59311e5f7278053e3a39c52953add079452
Keijiro Takahashi <keijiro@gmail.com>
2023-06-05 10:31:39 +0900

    :wrench: refactoring

--- Assets/Scripts/SpawnerSystem.cs
+++ Assets/Scripts/SpawnerSystem.cs
@@ -14,51 +14,60 @@ public partial struct SpawnerSystem : ISystem
             RunSpawner(ref state, xform.ValueRO, spawner.ValueRO);
     }
 
-    void RunSpawner(ref SystemState state, in LocalTransform xform, in Spawner spawner)
+    void RunSpawner
+      (ref SystemState state, in LocalTransform xform, in Spawner spawner)
     {
         // Total count of rays
         var total = spawner.Resolution.x * spawner.Resolution.y;
 
         // Raycast struct array
-        using var raycastBuffer = new NativeArray<RaycastCommand>(total, Allocator.TempJob);
-        var raycasts = new NativeSlice<RaycastCommand>(raycastBuffer);
+        using var rays = new NativeArray<RaycastCommand>(total, Allocator.TempJob);
+        using var hits = new NativeArray<RaycastHit>(total, Allocator.TempJob);
 
-        for (var (i, iy) = (0, 0); iy < spawner.Resolution.y; iy++)
+        // Raycast setup
         {
-            for (var ix = 0; ix < spawner.Resolution.x; ix++, i++)
+            var slice = new NativeSlice<RaycastCommand>(rays);
+            var dir = math.float3(0, 0, 1);
+            var dist = spawner.Extent.z * 2;
+            var idx = 0;
+
+            for (var iy = 0; iy < spawner.Resolution.y; iy++)
             {
-                var u = (float)ix / (spawner.Resolution.x - 1);
-                var v = (float)iy / (spawner.Resolution.y - 1);
+                for (var ix = 0; ix < spawner.Resolution.x; ix++)
+                {
+                    var u = (float)ix / (spawner.Resolution.x - 1);
+                    var v = (float)iy / (spawner.Resolution.y - 1);
 
-                var x = math.lerp(-spawner.Extent.x, spawner.Extent.x, u);
-                var y = math.lerp(-spawner.Extent.y, spawner.Extent.y, v);
+                    var x = math.lerp(-spawner.Extent.x, spawner.Extent.x, u);
+                    var y = math.lerp(-spawner.Extent.y, spawner.Extent.y, v);
 
-                var p = xform.Position + math.float3(x, y, -spawner.Extent.z);
+                    var p = xform.Position + math.float3(x, y, -spawner.Extent.z);
 
-                raycasts[i] = new RaycastCommand
-                  (p, math.float3(0, 0, 1), spawner.Extent.z * 2, 0x7fffffff, 1);
+                    slice[idx++] = new RaycastCommand(p, dir, QueryParameters.Default, dist);
+                }
             }
         }
 
-        var hits = new NativeArray<RaycastHit>(total, Allocator.TempJob);
+        // Raycast batch
+        RaycastCommand.ScheduleBatch(rays, hits, 16).Complete();
 
-        RaycastCommand.ScheduleBatch(raycastBuffer, hits, 16).Complete();
-
+        // Hit count
         var count = 0;
         for (var i = 0; i < total; i++)
             if (hits[i].collider != null) count++;
 
-        UnityEngine.Debug.Log(count);
-
-        var prefab = spawner.Prefab;
-        var instances = state.EntityManager.Instantiate(prefab, count, Allocator.Temp);
-
-        var idx = 0;
-        for (var i = 0; i < total; i++)
+        // Mass instantiation
         {
-            if (hits[i].collider == null) continue;
-            var xformc = SystemAPI.GetComponentRW<LocalTransform>(instances[idx++]);
-            xformc.ValueRW.Position = hits[i].point;
+            var prefab = spawner.Prefab;
+            var instances = state.EntityManager.Instantiate(prefab, count, Allocator.Temp);
+
+            var idx = 0;
+            for (var i = 0; i < total; i++)
+            {
+                if (hits[i].collider == null) continue;
+                var xformc = SystemAPI.GetComponentRW<LocalTransform>(instances[idx++]);
+                xformc.ValueRW.Position = hits[i].point;
+            }
         }
     }
 }
